15730bb9c3031a7677f1f0197bf388cca2d7a2de
CondorPepinillo <marttapia2023@udec.cl>
2024-05-12 17:12:17 -0400

    cambios a Nota

--- demo/src/main/java/com/tarea2doo/Nota.java
+++ demo/src/main/java/com/tarea2doo/Nota.java
@@ -2,4 +2,11 @@ package com.tarea2doo;
 
 public class Nota {
     private String contenido;
+    public  Nota(String contenido){
+        this.contenido = contenido;
+    }
+
+    public String getContenido() {
+        return contenido;
+    }
 }
