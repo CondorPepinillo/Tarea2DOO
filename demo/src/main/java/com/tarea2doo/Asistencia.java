282e61568e0e0997a24b90db7c1e101c27bc467e
martinezudec <benjamartinez2023@udec.cl>
2024-05-13 19:06:44 -0400

    feat: Implementación de la funcionalidad de Asistencia y retraso
    Creación de Atributos, contructor y getters de Asistencia y se terminó de implementar el ingreso de los Retrasos

--- demo/src/main/java/com/tarea2doo/Asistencia.java
+++ demo/src/main/java/com/tarea2doo/Asistencia.java
@@ -1,5 +1,14 @@
 package com.tarea2doo;
 
 public class Asistencia {
-    
+    private final Empleado empleado;
+
+    public Asistencia(Empleado empleado) {
+        this.empleado = empleado;
+    }
+
+    public Empleado getEmpleado() {
+        return empleado;
+    }
+
 }
