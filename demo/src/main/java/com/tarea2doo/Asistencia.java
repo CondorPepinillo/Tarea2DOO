ed43e5984ec27d08c759de426b5220861c450e59
CondorPepinillo <marttapia2023@udec.cl>
2024-05-13 20:03:46 -0400

    revert

--- demo/src/main/java/com/tarea2doo/Asistencia.java
+++ demo/src/main/java/com/tarea2doo/Asistencia.java
@@ -1,14 +1,5 @@
 package com.tarea2doo;
 
 public class Asistencia {
-    private final Empleado empleado;
-
-    public Asistencia(Empleado empleado) {
-        this.empleado = empleado;
-    }
-
-    public Empleado getEmpleado() {
-        return empleado;
-    }
-
+    
 }
--- demo/src/main/java/com/tarea2doo/Retraso.java
+++ demo/src/main/java/com/tarea2doo/Retraso.java
@@ -3,12 +3,11 @@ package com.tarea2doo;
 import java.time.Instant;
 
 public class Retraso extends Asistencia{
-    private final Instant hora;
-    public Retraso(Empleado empleado, Instant hora) {
-        super(empleado);
-        this.hora = hora;
-    }
+    Instant hora =Instant.now();
     public Instant getHora() {
         return hora;
+    }
+    public void setHora(Instant hora) {
+        this.hora = hora;
     }
 }
