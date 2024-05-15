e410ee3182d1b6b326276f70a210c17f64f2423b
CondorPepinillo <marttapia2023@udec.cl>
2024-05-15 18:37:32 -0400

    Se modifico el MainTest para hacerlo mas util

--- demo/src/test/java/com/tarea2doo/MainTest.java
+++ demo/src/test/java/com/tarea2doo/MainTest.java
@@ -1,18 +1,60 @@
 package com.tarea2doo;
 
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalTime;
+import java.util.Date;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
+    Empleado empleado1 = new Empleado("1","funlanito", "juarez"," fj@udec.cl");
+    Empleado empleado2 = new Empleado("2","Pedro", "martinez"," pedroMar@hotmail.cl");
+    Empleado empleado3 = new Empleado("3", "eduardo", "tapia", "eduTap@udec.cl");
+    Empleado empleado4 = new Empleado("1","funlanito", "juarez"," fj@udec.cl");
+    Empleado empleado5 = new Empleado("2","Pedro", "martinez"," pedroMar@hotmail.cl");
+    Empleado empleado6 = new Empleado("3", "eduardo", "tapia", "eduTap@udec.cl");
 
-    @org.junit.jupiter.api.BeforeEach
-    void setUp() {
+    Departamento depa1 = new Departamento("depa1");
+    @Test
+    void CantidadDeEmpleadosCaso3Empleados(){
+        depa1.addEmpleado(empleado4);
+        depa1.addEmpleado(empleado5);
+        depa1.addEmpleado(empleado6);
+
+        assertEquals(3, depa1.getCantidadEmpleados());
     }
 
-    @org.junit.jupiter.api.AfterEach
-    void tearDown() {
-    }
+    @Test
+    void getTipoReunion(){
+        Date hoy = new Date();
+        Instant horaPrevista = Instant.now();
 
-    @org.junit.jupiter.api.Test
-    void main() {
+        LocalTime inicio = LocalTime.of(13, 0); // Hora de inicio
+        LocalTime fin = LocalTime.of(14, 30);
+        Duration duracion = Duration.between(inicio, fin);
+
+        ReunionPresencial reunion1 = new ReunionPresencial("401",tipoReunion.MARKETING, hoy, horaPrevista, duracion);
+
+        empleado1.invitar(reunion1);
+        empleado2.invitar(reunion1);
+        empleado3.invitar(reunion1);
+
+        depa1.invitar(reunion1);
+
+        Asistencia asiste1 = new Asistencia(empleado1);
+        Ausencia noasiste1 = new Ausencia(empleado2);
+
+
+        reunion1.obtenerAsistencias().add(asiste1);
+
+        List<Asistencia> asistencias = reunion1.obtenerAsistencias();
+
+        assertEquals(tipoReunion.MARKETING, reunion1.getTipoReunion());
     }
+    @Test
+
 }
